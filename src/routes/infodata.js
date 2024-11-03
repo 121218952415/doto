7747c4fc95b713db1cfdc79c28cd2af81e9e721d
121218952415 <adrian.alexis.vega@gmail.com>
2024-11-02 18:02:24 -0600

    algoritmo

--- src/routes/infodata.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router(); // Asegúrate de invocar la función Router()
-const fetchItems = require("../controllers/getinfo");
-
-router.get("/", fetchItems);
- 
-
-module.exports = router;
--- src/routes/infoget.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const filterItems = require("../controllers/getfiltermeli");
-const router = express.Router(); // Asegúrate de invocar la función Router()
-
-router.get("/",filterItems );
- 
-
-module.exports = router;
