aa6bba1c1f7465f7ceab0f86dad806b949ee1441
121218952415 <adrian.alexis.vega@gmail.com>
2024-11-02 18:02:11 -0600

    algoritmos

---
+++ src/invertircadena.js
@@ -0,0 +1,27 @@
+// palabras = ["Hola  como te llamas ", "este es una prueba "];
+
+
+// function redondeoCalificacion(palabras) {
+//   let resultado = "";
+//   if (palabras.length === 0) {
+//     console.log("falta texto ");
+//   } else {
+//     alreves = palabras.join().split("");
+//     console.log(alreves)
+//     for (let i = alreves.length - 1; i >= 0; i--) {
+//       resultado += alreves[i];
+//     }
+//     console.log(resultado)
+//   }
+//   return resultado;
+// }
+// // redondeoCalificacion(palabras);
+// redondeoCalificacion(palabras); // Imprime "aloh"
+
+
+let texto = "La programación es una habilidad valiosa en el mundo moderno. Aprender a codificar no solo te abre puertas laborales, sino que también fomenta el pensamiento crítico y la resolución de problemas.";
+
+
+function encontrarVocales (){
+  let vocales  
+}
